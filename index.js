a6efac1c5a85dbb5717a33f07b7095df9e90e19e
LucasNotaro <lucas.sn@live.com>
2024-11-02 16:02:04 -0300

    função para verificar o nível

---
+++ index.js
@@ -0,0 +1,11 @@
+function verificarNivel(vitorias) {
+    if (vitorias < 10) return "Ferro";
+    if (vitorias <= 20) return "Bronze";
+    if (vitorias <= 50) return "Prata";
+    if (vitorias <= 80) return "Ouro";
+    if (vitorias <= 90) return "Diamante";
+    if (vitorias <= 100) return "Lendário";
+    return "Imortal";
+}
+
+console.log(verificarNivel(90));
